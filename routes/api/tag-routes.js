03d443f1aa08f3c3036181fabfbe20725f20f624
Brandon Kilgore <brandonk1023@gmail.com>
2023-04-26 17:45:06 -0400

    finished adding code to tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,13 +6,44 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
-
+  Tag.findAll({
+    include: [
+      {
+        model: Product,
+      }
+    ]
+  })
+  .then(tagData => res.json(tagData))
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+      }
+    ]
+  })
+  .then(tagData => {
+    if (!tagData) {
+      res.status(404).json({message: 'No data found with id'})
+      return
+    }
+    res.json(tagData)
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
 });
 
 router.post('/', (req, res) => {
